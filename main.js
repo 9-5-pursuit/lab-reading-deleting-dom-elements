d199427213106c4df564efd965131d5510419a89
davep80 <davidpaq1@gmail.com>
2023-03-13 12:21:43 -0400

    formatting

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ ap.remove()
 
 let rp = document.querySelectorAll('p')
 
-rp.forEach(item => { if (item.textContent.includes('Read Time')) item.remove()})
+rp.forEach(item => { if (item.textContent.includes('Read Time')) item.remove() })
 
 let sp = document.querySelectorAll('article')
 
-sp[sp.length-1].remove()
+sp[sp.length - 1].remove()
 
 let rt = document.querySelectorAll('h3')
 
