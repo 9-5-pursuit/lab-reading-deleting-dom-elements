a7f19e98f6fa19ea55a15a650b7c3c2043efa393
RicDW <rawdevx21@gmail.com>
2023-03-14 09:25:12 -0400

    submission

---
+++ .history/main_20230314092115.js
@@ -0,0 +1,13 @@
+let removedAllPosts = document.querySelector("header ul li");
+removedAllPosts.remove();
+
+let removedSpan = document.querySelector(".featured span");
+removedSpan.remove();
+
+let removedLastPost = document.querySelectorAll("main .posts article");
+removedLastPost[2].remove();
+
+let arr = document.querySelectorAll("main .posts h3");
+let titles = arr.forEach((title) => {
+  title.remove();
+});
