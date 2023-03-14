b991d8bc706426451417a9dee901ec003b88cba1
thandi bristol <thandibristol@thandis-air.mynetworksettings.com>
2023-03-14 10:41:26 -0400

    done

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 let post = document.querySelector("header nav ul li" );
 post.remove();
 
-let span = document.querySelector("main span");
+let span = document.querySelector(".featured article aside p span");
 span.remove();
 
-let lastPost = document.querySelectorAll(".post article")
-let stopPost = lastPost[2];
-stopPost.remove();
+let lastPost = document.querySelector(" .posts article:last-child")
+lastPost.remove();
 
-let allTitles= document.querySelectorAll("main .post, h3")
+let allTitles= document.querySelectorAll("main .posts, h3")
 allTitles.forEach((element)=>element.remove)
