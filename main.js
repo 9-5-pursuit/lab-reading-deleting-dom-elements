9be88b82b535156c228358796e3dfd34e249e0b2
Michael Dwayne Crayton <mdc@Michaels-Air.lan>
2023-03-20 12:34:30 -0400

    removed allposts link and read element from page

---
+++ main.js
@@ -0,0 +1,4 @@
+let allPosts = document.querySelector("li");
+allPosts.remove(); // removed "All Posts" element from header
+let readTime = document.querySelector("aside span");
+readTime.remove(); // removed "Read Time" element from aside span
